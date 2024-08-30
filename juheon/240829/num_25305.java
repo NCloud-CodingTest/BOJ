11227b3cac44f7acb98f5806ac3286e51f770145
PNT-01 <sjh98127@gmail.com>
2024-08-30 10:06:49 +0900

    25305 업로드

---
+++ juheon/240829/num_25305.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class num_25305 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] arr = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int result = arr[N - k];
+
+        System.out.println(result);
+
+        sc.close();
+		}
+	} 
+
