5222f1e22325d6b34c15f8d70b54c34f93269c69
PNT-01 <sjh98127@gmail.com>
2024-09-02 16:01:16 +0900

    .

---
+++ juheon/240901/num_24262.java
@@ -0,0 +1,11 @@
+package team_baek;
+
+public class num_24262 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println(1);
+			System.out.println(0);
+	}
+
+}
