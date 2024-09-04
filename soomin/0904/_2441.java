a457d062f595b013fe262ea9a593fb7fdd29eebf
WIN-HR35D92QS4P\soominchoi <csm0062@naver.com>
2024-09-04 11:25:44 +0900

    0904

---
+++ soomin/0904/_2441.java
@@ -0,0 +1,26 @@
+package baek;
+
+import java.util.Scanner;
+
+public class _2441 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i = 0; i < n; i++) {
+			
+			for(int j = 0; j < n; j++) {
+				
+				if(i <= j) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			} 
+			
+			System.out.println();
+		}
+	}
+}
