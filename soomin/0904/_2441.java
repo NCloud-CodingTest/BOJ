aec75066814eb4c1bef8192d8c8f264d0ba5bdb3
WIN-HR35D92QS4P\soominchoi <csm0062@naver.com>
2024-09-04 18:02:16 +0900

    no message

--- soomin/0904/_2441.java
+++ soomin/0904/_2441.java
@@ -6,21 +6,17 @@ public class _2441 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		int n = sc.nextInt();
-		
-		for(int i = 0; i < n; i++) {
-			
-			for(int j = 0; j < n; j++) {
-				
-				if(i <= j) {
-					System.out.print("*");
-				} else {
-					System.out.print(" ");
-				}
-			} 
-			
-			System.out.println();
-		}
+		for(int i = 0; i < n; i++) 
+		System.out.println(" ".repeat(i) + "*".repeat(n-i));
+//		for(int i = 0; i < n; i++) {
+//		for(int j = 0; j < n; j++) {
+//			if(i <= j) {
+//				System.out.print("*");
+//			} else {
+//				System.out.print(" ");
+//			}
+//		} System.out.println();
+//	}
 	}
 }
